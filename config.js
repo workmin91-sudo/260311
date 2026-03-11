180262cd40d9cc22e399d84e065f26c6627ccf1f
workmin91-sudo <workmin91-sudo@users.noreply.github.com>
2026-03-11 15:14:45 +0900

    config.js 異붽? - GitHub Pages?먯꽌 Supabase ?곌껐

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase 설정
+const SUPABASE_CONFIG = {
+    url: 'https://xxzdkqhmxlfgjwyhpvch.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4emRrcWhteGxmZ2p3eWhwdmNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxOTc1MzAsImV4cCI6MjA4ODc3MzUzMH0.m5pLhNybJPvoGbBe5mxbLBsWO7a6iKRedQ_o8j4HONs'
+};
